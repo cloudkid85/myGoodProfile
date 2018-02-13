95a2f2189156a35d24e41363ec7975a87e3104b9
cloudkid <ignacioarjona85@gmail.com>
2018-02-13 22:15:33 +0100

    initial config

---
+++ src/main/java/test/controladores/GeneralControllerTest.java
@@ -0,0 +1,21 @@
+package test.controladores;
+
+import es.iarjona.controladores.GeneralController;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+
+public class GeneralControllerTest {
+
+    @Test
+    public void testLoginPage() throws Exception {
+        GeneralController controller = new GeneralController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+}
